e66b97d5d7238eb18ef312df51337e5f855e302c
Ian Kimble <ian.kimble@eRT.com>
2020-02-02 22:43:53 -0500

    get

--- server.js
+++ server.js
@@ -16,6 +16,14 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/", contactRoute);
+app.use(express.static(path.join(__dirname, "client", "build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 // Port
 const port = process.env.PORT || 7000;
